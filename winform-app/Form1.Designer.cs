a4d154a180a17380c39394f084bd6ff1cb86eed2
tomasc5-create <tomasc5@hotmail.com>
2026-09-15 16:57:42 -0300

    Hola

--- winform-app/Form1.Designer.cs
+++ winform-app/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.Modificar = new System.Windows.Forms.Button();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.Agregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(12, 12);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(776, 217);
+            this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvArticulos_CellContentClick);
+            // 
+            // Modificar
+            // 
+            this.Modificar.Location = new System.Drawing.Point(346, 266);
+            this.Modificar.Name = "Modificar";
+            this.Modificar.Size = new System.Drawing.Size(75, 23);
+            this.Modificar.TabIndex = 1;
+            this.Modificar.Text = "Modificar";
+            this.Modificar.UseVisualStyleBackColor = true;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.Location = new System.Drawing.Point(427, 266);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 2;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Agregar
+            // 
+            this.Agregar.Location = new System.Drawing.Point(265, 266);
+            this.Agregar.Name = "Agregar";
+            this.Agregar.Size = new System.Drawing.Size(75, 23);
+            this.Agregar.TabIndex = 3;
+            this.Agregar.Text = "Agregar";
+            this.Agregar.UseVisualStyleBackColor = true;
+            this.Agregar.Click += new System.EventHandler(this.Agregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Agregar);
+            this.Controls.Add(this.Eliminar);
+            this.Controls.Add(this.Modificar);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "Form1";
+            this.Text = "Gestión de Artículos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.Button Modificar;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button Agregar;
     }
 }
 
--- winform-app/Form1.cs
+++ winform-app/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dominio;
 
 namespace winform_app
 {
@@ -15,6 +16,38 @@ namespace winform_app
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //mi part archulo 1 y 2 se borran ya que son de prueba
+            List<Articulo> listaPrueba = new List<Articulo>();
+
+            Articulo articulo1 = new Articulo();
+            articulo1.Nombre = "PlayStation 5";
+            articulo1.Descripcion = "Consola de videojuegos";
+            articulo1.Precio = 500000;
+
+            Articulo articulo2 = new Articulo();
+            articulo2.Nombre = "Mouse Inalámbrico";
+            articulo2.Descripcion = "Mouse para PC";
+            articulo2.Precio = 25000;
+
+            listaPrueba.Add(articulo1);
+            listaPrueba.Add(articulo2);
+
+            dgvArticulos.DataSource = listaPrueba;
+
+        }
+
+        private void dgvArticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void Agregar_Click(object sender, EventArgs e)
+        {
+            frmAltaArticulo ventanaAlta = new frmAltaArticulo();
+            ventanaAlta.ShowDialog();
         }
     }
 }
